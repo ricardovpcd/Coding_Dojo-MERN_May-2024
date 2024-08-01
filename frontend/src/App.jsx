a264d0c022ffa2703d7f33ff4795f317fafad1fd
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2024-07-31 19:41:37 -0500

    yup file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import styles from "./myApp.module.css";
 import axios from "axios";
+import { object, string, number } from 'yup';
 
 const App = () => {
   const [listProducts, setListProducts] = useState([]);
+  const [productName, setProductName] = useState("");
+  const [productSku, setProductSku] = useState("");
 
   useEffect(()=>{
     getProducts();
@@ -23,8 +26,47 @@ const App = () => {
     getProducts();
   }
 
+  const createProduct = async () => {
+
+    let productSchema = object({
+      name: string().required(),
+      sku: number().required().positive().integer()
+    });
+
+    var data = {
+      "name": productName,
+      "sku": productSku,
+      "isAvailable": true
+    };
+
+    var valid = await productSchema.isValid(data);
+    if(!valid){
+      alert("Error en el formulario");
+      return;
+    }
+
+    var response = await axios.post("http://localhost:8080/product/create", data);
+    setProductName("");
+    setProductSku("");
+
+    var newProduct = response.data;
+
+    var listProductsTemp = [...listProducts];
+    listProductsTemp.push(newProduct);
+    setListProducts(listProductsTemp);
+  }
+
   return (
     <div>
+      <div>
+        <label>Nombre: </label>
+        <input type="text" onChange={(e) => setProductName(e.target.value)} value={productName}/>
+        <label>SKU: </label>
+        <input type="number" onChange={(e) => setProductSku(e.target.value)} value={productSku}/>
+
+        <button onClick={createProduct}>Crear producto</button>
+      </div>
+      <br />
       <div className={styles.productsParent}>
 
         {
