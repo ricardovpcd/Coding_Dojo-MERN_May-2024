c1376509bfebd0788e2d4dcfeb7b57ef22488653
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2024-08-07 20:08:54 -0500

    test revert

--- backend/controllers/pirateController.js
+++ backend/controllers/pirateController.js
@@ -21,4 +21,10 @@ const deletePirate = async (req, res) => {
     res.status(200).json();
 }
 
+const updatePirate = async (req, res) => {
+    var id = req.params.pirateId;
+    await Pirate.findByIdAndDelete(id);
+    res.status(200).json();
+}
+
 export {createPirate, getAllPirates, deletePirate};
